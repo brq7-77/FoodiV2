a3e27146a24f174f281a1111ace1048ac6c810c2
brq7-77 <barqanesports@gmail.com>
2026-05-25 03:23:26 +0300

    fix project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./router";
 import { I18nextProvider } from "react-i18next";
@@ -20,7 +20,7 @@ function LanguageDirection() {
 function App() {
   return (
     <I18nextProvider i18n={i18n}>
-      <BrowserRouter basename={__BASE_PATH__}>
+      <BrowserRouter>
         <LanguageDirection />
         <AppRoutes />
       </BrowserRouter>
